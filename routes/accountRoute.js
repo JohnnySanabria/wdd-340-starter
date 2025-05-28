c4a946413a8de7ac115b7b35294d5bc7023b44af
JohnnyASan <johnanthonysanabria@gmail.com>
2025-05-27 19:23:58 -0600

    registration successfully processed

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = new express.Router();
+const utilities = require("../utilities/");
 const accountController = require("../controllers/accountController");
 
 router.get("/login", accountController.buildLogin);
 router.get("/register", accountController.buildRegister);
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
+router.post(
+  "/register",
+  utilities.handleErrors(accountController.registerAccount)
+);
 
 module.exports = router;
