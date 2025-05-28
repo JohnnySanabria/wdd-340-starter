f7aa668e68b400687c009640bed2e3e789f86a77
Johnny <johnanthonysanabria@gmail.com>
2025-05-27 18:52:44 -0600

    basic register form delivered

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -3,5 +3,6 @@ const router = new express.Router();
 const accountController = require("../controllers/accountController");
 
 router.get("/login", accountController.buildLogin);
+router.get("/register", accountController.buildRegister);
 
 module.exports = router;
